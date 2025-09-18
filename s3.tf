cecac0e3975f321c237f7ff2367448c20670ad12
Sashangykumar <sashangykumar1628@gmail.com>
2025-09-18 18:19:01 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,1 @@
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "vk-terraform-state-20250918" # change to unique name
 
-  tags = {
-    Name        = "terraform-state-bucket"
-    Environment = "dev"
-  }
-}
