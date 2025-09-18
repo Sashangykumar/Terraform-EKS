079a3208e89c163b11dbaaba988693a0d0c0a98a
Sashangykumar <sashangykumar1628@gmail.com>
2025-09-18 18:11:08 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "vk-terraform-state-20250918" # same as above
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
+
